c314dcd6f6f6591908cb5a2b16c92370e34f04b4
Bryan Helms <bryan.helms@microsoft.com>
2018-06-10 00:04:52 -0700

    Realize I was not thinking about the iterator enough

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -2,12 +2,5 @@ extern crate unicode_segmentation;
 use unicode_segmentation::{UnicodeSegmentation};
 
 pub fn reverse(input: &str) -> String {
-    // Let's be memory hungry, and just have a new String we'll push into. (We can't
-    // rewrite the existing string anyway).
-    let mut reversed = String::new();
-    for grapheme in UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>().iter().rev() {
-        reversed.push_str(grapheme);
-    }
-
-    return reversed;
+    return UnicodeSegmentation::graphemes(input, true).rev().collect();
 }
