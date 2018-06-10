5a2c238f2c836b87f8ccf57a1fb94b4cf12c518d
Bryan Helms <bryan.helms@microsoft.com>
2018-06-09 18:31:12 -0700

    Inlined the if statement inside the format! macro

--- rust/twofer/src/lib.rs
+++ rust/twofer/src/lib.rs
@@ -1,10 +1,3 @@
 pub fn twofer(name: &str)-> String {
-    // Saw this in someone else's solution. This is more like how I actually wanted to solve it.
-    let name_to_print = if name.is_empty() {
-        "you"
-    } else {
-        name
-    };
-
-    return format!("One for {}, one for me.", name_to_print);
+    return format!("One for {}, one for me.", if name.is_empty() { "you" } else { name });
 }
