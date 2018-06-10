35ce650d301d040a55ce2734ce4497c7703729e4
Bryan Helms <bryan.helms@microsoft.com>
2018-06-09 22:23:20 -0700

    Checking out closures

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,5 @@
 pub fn is_leap_year(year: i32) -> bool {
-    return year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0);
+    // Wanted to check out closures
+    let is_divisible_by = |num: i32| -> bool { year % num == 0 };
+    return is_divisible_by(4) && (!is_divisible_by(100) || is_divisible_by(400));
 }
