4bee675d60b2ef160fa5689c4ec37e7954922590
Bryan Helms <bryan.helms@microsoft.com>
2018-06-09 18:28:54 -0700

    Incorporate what I found on exercism.io

--- rust/twofer/src/lib.rs
+++ rust/twofer/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn twofer(name: &str)-> String {
-    // Using a mutable variable.
-    let mut name_to_print = name;
-    if name_to_print.is_empty() {
-        name_to_print = "you";
-    }
+    // Saw this in someone else's solution. This is more like how I actually wanted to solve it.
+    let name_to_print = if name.is_empty() {
+        "you"
+    } else {
+        name
+    };
 
     return format!("One for {}, one for me.", name_to_print);
 }
