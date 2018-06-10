c596277ca5b76396bcce49a0cd77f8cd7f14fec2
Bryan Helms <bryan.helms@microsoft.com>
2018-06-09 18:04:48 -0700

    Twofer solution 2

--- rust/twofer/src/lib.rs
+++ rust/twofer/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn twofer(name: &str)-> String {
+    // Feels naive, not sure if this results in few allocations either.
+    // Trying to avoid a mutable variable.
+    let name_to_print;
     if name.is_empty() {
-        return format!("One for you, one for me.");
+        name_to_print = "you";
+    } else {
+        name_to_print = name;
     }
 
-    return format!("One for {}, one for me.", name);
+    return format!("One for {}, one for me.", name_to_print);
 }
