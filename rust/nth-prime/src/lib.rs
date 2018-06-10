ad2ee39009e1b9ff86776b20f6adb42d1cf23a4c
Bryan Helms <bryan.helms@microsoft.com>
2018-06-10 10:48:18 -0700

    Actually write a solution for nth prime this time

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,15 +1,23 @@
-extern crate primes;
-use primes::PrimeSet;
-
 pub fn nth(n: u32) -> Option<u32> {
-    // Go stumped, caved, and used a library. Just submitting so I can see others' solutions.
-    if n == 0 { return Option::None; }
-
-    let mut pset = PrimeSet::new();
-    let mut found_prime: Option<u32> = Option::None;
-    for (_, n) in pset.iter().enumerate().skip((n - 1) as usize).take(1) {
-        found_prime = Some(n as u32);
+    // Okay, time to try this again
+    match n {
+        // Some easy wins first
+        n if n == 0 => None,
+        n if n == 1 => Option::from(2),
+        n if n == 2 => Option::from(3),
+        // Just start going through all the numbers and pull the nth prime. Really inefficient
+        n => (1..).filter(|c| is_prime(*c)).nth(n as usize)
     }
+}
 
-    return found_prime;
+fn is_prime(candidate: u32) -> bool {
+    // Do a basic primality test, as described here https://en.wikipedia.org/wiki/Primality_test
+    // I saw someone else do the logic similar to this, and for the longest time I was stumped on
+    // why the line started with !. I was looking so hard, assuming it was some kind of weird
+    // Rust operator. It finally clicked for me though. It's just boolean logic, nothing special.
+    //
+    // In a nutshell, it's MUCH more likely that we'll find a divisor for a number than we won't,
+    // so the .any is returning true if it finds a divisor that has no remainder. Then it's just
+    // bitwise complemented (notted). :facepalm: to me.
+    !(2..(candidate as f64).sqrt().floor() as u32 + 1).any(|divisor| candidate % divisor == 0)
 }
