47669e86c227b93a91260ba799a1a9fcd6f91d68
Bryan Helms <bryan.helms@microsoft.com>
2018-06-10 13:04:20 -0700

    Clean up the Bob solution a little bit

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -10,12 +10,11 @@ pub fn reply(message: &str) -> &str {
 }
 
 fn analyze_message(message: &str) -> (bool, bool, bool) {
-    return if message.is_empty() { (true, false, false) }
-        else {
-        let mut alphabetics = message.chars().filter(|c| c.is_alphabetic());
-        let is_yelled = alphabetics.any(|_| true) && alphabetics.all(|c| c.is_uppercase());
+    return {
+        let is_empty = message.trim().is_empty();
+        let is_yelled = message.chars().any(|c| c.is_alphabetic())
+            && message.chars().all(|c| c.is_uppercase() || !c.is_alphabetic());
         let is_question = message.trim_right().chars().last() == Some('?');
-        let is_empty = message.chars().all(|c| c.is_whitespace() || c.is_control());
 
         (is_empty, is_yelled, is_question)
     }
